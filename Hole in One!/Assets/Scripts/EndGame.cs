81aeca2a9e3e60215bc18f4076ac39a9d9874394
M7UNDO <dhlaminimfundo1@gmail.com>
2025-03-11 23:44:02 +0200

    Golf Course Update

--- Hole in One!/Assets/Scripts/EndGame.cs
+++ Hole in One!/Assets/Scripts/EndGame.cs
@@ -7,14 +7,13 @@ using TMPro;
 public class EndGame : MonoBehaviour
 {
     public GameObject EndPanel;
-    public GameObject closeBtn;
 
     private void OnCollisionEnter(Collision coli)
     {
         if (coli.gameObject.CompareTag("Player"))
         {
             EndPanel.SetActive(true);
-            closeBtn.SetActive(false);
+           
 
         }
         
