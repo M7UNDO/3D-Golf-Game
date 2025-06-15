81ca57deab42689e5c2bfff54885e131776dc2ec
M7UNDO <dhlaminimfundo1@gmail.com>
2025-06-15 22:36:36 +0200

    Single Player issue Test

--- Hole in One!/Assets/Scripts/Movement/InitialSpawn.cs
+++ Hole in One!/Assets/Scripts/Movement/InitialSpawn.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class InitialSpawn : MonoBehaviour
 {
     public GameObject prefab;
+    public bool isSinglePlayer = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        /*
         var player1 = PlayerInput.Instantiate(prefab, 0, "WASD", 0, Keyboard.current );
         var player2 = PlayerInput.Instantiate(prefab, 0, "Arrows", 0, Keyboard.current );
 
@@ -20,6 +23,37 @@ public class InitialSpawn : MonoBehaviour
 
         player1.GetComponent<PlayerMovement>().playerIndex = 0;
         player2.GetComponent<PlayerMovement>().playerIndex = 1;
+        */
+
+        if (!isSinglePlayer)
+        {
+            var player1 = PlayerInput.Instantiate(prefab, 0, "WASD", 0, Keyboard.current);
+            var player2 = PlayerInput.Instantiate(prefab, 0, "Arrows", 0, Keyboard.current);
+
+            player1.transform.position = new Vector3(-3.642f, 0.399f, -6.98f);
+            player2.transform.position = new Vector3(-6f, 0.399f, -6.98f);
+
+            player1.transform.parent.GetChild(1).GetComponent<Camera>().rect = new Rect(0, 0, .5f, 1);
+            player2.transform.parent.GetChild(1).GetComponent<Camera>().rect = new Rect(.5f, 0, .5f, 1);
+
+            player1.transform.parent.GetChild(1).GetComponent<AudioListener>().enabled = true;
+
+            player1.GetComponent<PlayerMovement>().playerIndex = 0;
+            player2.GetComponent<PlayerMovement>().playerIndex = 1;
+        }
+        else if (isSinglePlayer)
+        {
+            var player1 = PlayerInput.Instantiate(prefab, 0, "WASD", 0, Keyboard.current);
+
+            player1.transform.position = new Vector3(-3.642f, 0.399f, -6.98f);
+
+            player1.transform.parent.GetChild(1).GetComponent<Camera>().rect = new Rect(0, 0, 1, 1);
+
+            player1.transform.parent.GetChild(1).GetComponent<AudioListener>().enabled = true;
+
+            player1.GetComponent<PlayerMovement>().playerIndex = 0;
+            Debug.Log(player1);
+        }
     }
 
 }
